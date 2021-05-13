48dcf0fe5d15ea21690460e6366ac1e8228b152b
Dinty1 <dintybmrt@gmail.com>
2021-05-13 23:14:31 +0100

    Will add actual stuff tomorrow

--- src/main/java/io/github/mrt_map/mappy/Mappy.java
+++ src/main/java/io/github/mrt_map/mappy/Mappy.java
@@ -1,4 +1,20 @@
 package io.github.mrt_map.mappy;
 
-public class Mappy { // TODO make this do shit
+import javax.security.auth.login.LoginException;
+
+import lombok.Getter;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Mappy {
+
+    public Mappy(String token) throws LoginException {
+        JDABuilder jda = JDABuilder.create(token, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES);
+
+        jda.build();
+    }
+    @SuppressWarnings("unused")
+    public static void main(String[] args) throws LoginException {
+        new Mappy(args[0]); // Init with token
+    }
 }
