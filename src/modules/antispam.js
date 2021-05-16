93de024817d1806676a486fdb348d442b77676aa
Dinty1 <dintybmrt@gmail.com>
2021-05-16 12:18:33 +0100

    Ban users who send 12 mentions in 15 seconds

--- src/modules/antispam.js
+++ src/modules/antispam.js
@@ -6,19 +6,31 @@ module.exports = {
         // Fast user message spam thing
 
         if (!module.exports.recentUserMessages[message.author.id]) module.exports.recentUserMessages[message.author.id] = [];
-        module.exports.recentUserMessages[message.author.id].push(message.id);
+        module.exports.recentUserMessages[message.author.id].push(message);
 
         if (module.exports.recentUserMessages[message.author.id].length == Math.ceil(message.client.config.userMessageSpamThreshold / 2)) {
             message.reply("slow down your messages! You're pretty close to being banned for spam.");
         } else if (module.exports.recentUserMessages[message.author.id].length >= message.client.config.userMessageSpamThreshold) {
-            message.guild.members.ban(message.author, {reason: `Sent ${module.exports.recentUserMessages[message.author.id].length} messages in ${message.client.config.userMessageSpamThresholdMilliseconds / 1000} seconds.`, days: 1}).then(banned => {
+            message.guild.members.ban(message.author, { reason: `Sent ${module.exports.recentUserMessages[message.author.id].length} messages in ${message.client.config.userMessageSpamThresholdMilliseconds / 1000} seconds.`, days: 1 }).then(banned => {
                 message.channel.send(`**${banned.tag}** (${banned}) has been banned for spam (sent ${module.exports.recentUserMessages[message.author.id].length} messages in ${message.client.config.userMessageSpamThresholdMilliseconds / 1000} seconds).`);
+            });
+        }
+
+        // Anti mass-mention thing
+        var totalMentions = 0;
+        for (const m of module.exports.recentUserMessages[message.author.id]) {
+            totalMentions += m.mentions.members.size;
+        }
+
+        if (totalMentions >= message.client.config.userMessageMentionSpamThreshold) {
+            message.guild.members.ban(message.author, { reason: `Mentioned ${totalMentions} users in ${message.client.config.userMessageSpamThresholdMilliseconds / 1000} seconds.`, days: 1 }).then(banned => {
+                message.channel.send(`**${banned.tag}** (${banned}) has been banned for spam (mentioned ${totalMentions} users in ${message.client.config.userMessageSpamThresholdMilliseconds / 1000} seconds).`);
             });
         }
 
         // Remove message from list after interval
         setTimeout(() => {
-            miscUtil.removeArrayElement(module.exports.recentUserMessages[message.author.id], message.id);
+            miscUtil.removeArrayElement(module.exports.recentUserMessages[message.author.id], message);
         }, message.client.config.userMessageSpamThresholdMilliseconds);
     }
 }
